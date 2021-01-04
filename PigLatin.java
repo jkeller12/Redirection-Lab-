b5c3a08785d6d6ac2d91c5a4e90cfaa5c83b0bbd
Jonah Keller <jkeller10@stuy.edu>
2021-01-04 13:12:41 -0500

    i did pig latin best

--- PigLatin.java
+++ PigLatin.java
@@ -2,14 +2,31 @@ import java.util.Arrays;
 public class PigLatin{
   public static String pigLatinSimple(String s)
   {
-    // given a word of a least 1 charAt
-    // if word starts in {a, e, i, o, u}
-    // --> add "hay" to the end of the word
-    // else
-    // --> move first letter to the end
-    // --> then add "ay" to the end
-    String Vowels = "aeiou";
     s = s.toLowerCase();
+    String Vowels = "aeiou";
+    if(-1 != Vowels.indexOf(s.charAt(0)))
+    {
+      s += "hay";
+    }
+    else
+    {
+      s += s.charAt(0);
+      s += "ay";
+      s = s.substring(1,s.length());
+    }
+    return s;
+
+  }
+
+
+
+
+
+
+  public static String pigLatin(String s)
+  {
+    s = s.toLowerCase();
+    String Vowels = "aeiou";
     String[] Diagraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
 
     String C01 = s.substring(0,2);
@@ -33,8 +50,34 @@ public class PigLatin{
     return s;
 
   }
+
+  public static String pigLatinBest(String s)
+  {
+    if(Character.isDigit(s.charAt(0)))
+    {
+      return s;
+    }
+    char Last_Value = s.charAt(s.length()-1);
+    boolean A = Character.isDigit(Last_Value);
+    boolean B = Character.isLetter(Last_Value);
+    if(!(A && B))
+    {
+      char Punctuation = Last_Value;
+      s = s.substring(0, s.length()-1);
+      s = pigLatin(s);
+      return s + Punctuation;
+    }
+    else
+    {
+      return pigLatin(s);
+    }
+
+  }
+
+
+
   public static void main(String[] args)
   {
-    System.out.println(pigLatinSimple("ThE"));
+    System.out.println(pigLatin("ThE"));
   }
 }
